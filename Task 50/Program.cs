2012318bde66a9993db3fd3e341eda22785cb1a4
Maksim <maksim_kabanov_83@mail.ru>
2022-12-03 23:24:36 +0300

    final solution improved

--- Task 50/Program.cs
+++ Task 50/Program.cs
@@ -10,14 +10,14 @@
 
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) // Задаем массив случайных целых чисел
 {
-    int[,] matrix = new int[rows, columns]; 
+    int[,] matrix = new int[rows, columns];
     Random rnd = new Random();
 
-    for (int i = 0; i < matrix.GetLength(0); i++) 
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            matrix[i, j] = rnd.Next(min, max + 1); 
+            matrix[i, j] = rnd.Next(min, max + 1);
         }
     }
     return matrix;
@@ -42,21 +42,21 @@ int row = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите индекс столбца элемента массива:");
 int column = Convert.ToInt32(Console.ReadLine());
 
-int [,] preDefinedMatrix = CreateMatrixRndInt(3,4,0,10);
+int[,] preDefinedMatrix = CreateMatrixRndInt(3, 4, 0, 10);
 Console.WriteLine("Задан массив простых чисел:");
 PrintMatrix(preDefinedMatrix);
 int element = 0;
 
-if (row < 0 || row > preDefinedMatrix.GetLength(0)-1) 
+if (row < 0 || row > preDefinedMatrix.GetLength(0) - 1)
 {
     Console.WriteLine("Элемента с указанным индексом не существует");
 }
-else if (column < 0 || column > preDefinedMatrix.GetLength(1)-1)
+else if (column < 0 || column > preDefinedMatrix.GetLength(1) - 1)
 {
     Console.WriteLine("Элемента с указанным индексом не существует");
 }
-else 
+else
 {
-    element = preDefinedMatrix[row,column];
+    element = preDefinedMatrix[row, column];
     Console.WriteLine($"Элемент с указанным индексом равен: {element}");
 }
